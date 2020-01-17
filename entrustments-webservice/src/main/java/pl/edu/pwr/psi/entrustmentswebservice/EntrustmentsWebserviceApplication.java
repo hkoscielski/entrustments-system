4d21106449c1ee7628341e7ba6014716c67f0cae
Hubert Kościelski <228172@student.pwr.edu.pl>
2020-01-17 23:09:48 +0100

    Initial commit

---
+++ entrustments-webservice/src/main/java/pl/edu/pwr/psi/entrustmentswebservice/EntrustmentsWebserviceApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pwr.psi.entrustmentswebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntrustmentsWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntrustmentsWebserviceApplication.class, args);
+	}
+
+}
---
+++ entrustments-webservice/src/test/java/pl/edu/pwr/psi/entrustmentswebservice/EntrustmentsWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pwr.psi.entrustmentswebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntrustmentsWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
