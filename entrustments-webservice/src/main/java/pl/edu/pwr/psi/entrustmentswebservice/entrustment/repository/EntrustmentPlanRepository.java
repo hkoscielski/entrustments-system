ff35b1a582a9af74121b0594f9e4bbefcac592d4
Hubert Kościelski <228172@student.pwr.edu.pl>
2020-02-05 18:25:39 +0100

    Backend - return semesterId with each entrustment

--- entrustments-webservice/src/main/java/pl/edu/pwr/psi/entrustmentswebservice/entrustment/repository/EntrustmentPlanRepository.java
+++ entrustments-webservice/src/main/java/pl/edu/pwr/psi/entrustmentswebservice/entrustment/repository/EntrustmentPlanRepository.java
@@ -1,6 +1,8 @@
 package pl.edu.pwr.psi.entrustmentswebservice.entrustment.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pl.edu.pwr.psi.entrustmentswebservice.entrustment.entity.EntrustmentPlan;
 
@@ -10,4 +12,7 @@ import java.util.Optional;
 public interface EntrustmentPlanRepository extends CrudRepository<EntrustmentPlan, Long> {
 
 	Optional<EntrustmentPlan> findBySemesterId(long semesterId);
+
+	@Query("select ep from EntrustmentPlan ep join fetch ep.semester where ep.id = :entrustment_plan_id")
+	Optional<EntrustmentPlan> findByEntrustmentPlanId(@Param("entrustment_plan_id") long entrustmentPlanId);
 }
