011feba914d0931ed06a3fc28431d03674346553
KJakubowski <konrad.mj08@gmail.com>
2020-02-07 03:54:50 +0100

    Created Dockerfiles

--- entrustments-webapp/src/environments/environment.prod.ts
+++ entrustments-webapp/src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiBaseUrl: "http://192.168.99.101:8080"
 };
--- entrustments-webapp/src/environments/environment.ts
+++ entrustments-webapp/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiBaseUrl: "http://localhost:8080"
+  apiBaseUrl: "http://192.168.99.101:8080"
 };
 
 /*
